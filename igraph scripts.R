c4f947ae62b1088d45f8509fa1789b4b6644d260
bengalengel <bengelmann9@gmail.com>
2014-07-15 09:49:42 -0500

    Add .R files igraph subsettingbd and regexp change

---
+++ igraph scripts.R
@@ -0,0 +1,133 @@
+install.packages("igraph")
+library(igraph)
+
+### produce degree distribution from single numerical vector
+
+data <- read.csv("degree table.csv")
+### grab high affinity subset
+High <- data$High.Affinity
+## Convert into a frequency table
+t <- table(High)
+
+## make the degree distribution (probability density function) plot
+plot(t/sum(t), xlab ="Degree", ylab="Frequency")
+
+##revised
+plot(t/sum(t), xlim=c(1,4), type='h', axes=FALSE, ann=FALSE)
+axis(1, at=1:4, lab=c(1:4))
+axis(2)
+title(xlab="Degree")
+title(ylab="Fraction of Peptides")
+
+
+> row <- rbind(c(1:4),total,high)
+> row
+[,1] [,2] [,3] [,4]
+1    2    3    4
+total    5   11   20   45
+high    31   25    7    0
+> col <- cbind(c(1:4),total,high)
+> col
+total high
+[1,] 1     5   31
+[2,] 2    11   25
+[3,] 3    20    7
+[4,] 4    45    0
+> fisher.test(col)
+Error in fisher.test(col) : FEXACT error 7.
+LDSTP is too small for this problem.
+Try increasing the size of the workspace.
+> 2e8
+[1] 2e+08
+> fisher.test(col,workspace=2e8)
+
+Fisher's Exact Test for Count Data
+
+data:  col 
+p-value < 2.2e-16
+alternative hypothesis: two.sided 
+
+> fisher.test(col,workspace=2e8, hybrid="True")
+
+Fisher's Exact Test for Count Data
+
+data:  col 
+p-value < 2.2e-16
+alternative hypothesis: two.sided 
+
+> fisher.test(col,workspace=2e8, hybrid="FALSE")
+
+Fisher's Exact Test for Count Data
+
+data:  col 
+p-value < 2.2e-16
+alternative hypothesis: two.sided 
+
+> fisher.test(col,workspace=2e8, hybrid="TRUE")
+
+Fisher's Exact Test for Count Data
+
+data:  col 
+p-value < 2.2e-16
+alternative hypothesis: two.sided 
+
+> fisher.test(row,workspace=2e8, hybrid="TRUE")
+
+Fisher's Exact Test for Count Data
+
+data:  row 
+p-value < 2.2e-16
+alternative hypothesis: two.sided 
+
+> fisher.test(col, simulate.p.value=TRUE, B=1e6)
+
+Fisher's Exact Test for Count Data with simulated p-value (based on 1e+06 replicates)
+
+data:  col 
+p-value = 1e-06
+alternative hypothesis: two.sided 
+
+> Job <- matrix(c(1,2,1,0, 3,3,6,1, 10,10,14,9, 6,7,12,11), 4, 4,
+                +               dimnames = list(income=c("< 15k", "15-25k", "25-40k", "> 40k"),
+                                                +                               satisfaction=c("VeryD", "LittleD", "ModerateS", "VeryS")))
+> Job
+satisfaction
+income   VeryD LittleD ModerateS VeryS
+< 15k      1       3        10     6
+15-25k     2       3        10     7
+25-40k     1       6        14    12
+> 40k      0       1         9    11
+> fisher.test(JOb)
+Error in is.data.frame(x) : object 'JOb' not found
+> fisher.test(Job)
+
+Fisher's Exact Test for Count Data
+
+data:  Job 
+p-value = 0.7827
+alternative hypothesis: two.sided 
+
+> fisher.test(Job, simulate.p.value=T, B=1e6)
+
+Fisher's Exact Test for Count Data with simulated p-value (based on 1e+06 replicates)
+
+data:  Job 
+p-value = 0.7829
+alternative hypothesis: two.sided 
+
+> fisher.test(row, simulate.p.value=TRUE, B=1e6)
+
+Fisher's Exact Test for Count Data with simulated p-value (based on 1e+06 replicates)
+
+data:  row 
+p-value = 1e-06
+alternative hypothesis: two.sided 
+
+> fisher.test(col, simulate.p.value=TRUE, B=1e7)
+
+	Fisher's Exact Test for Count Data with simulated p-value (based on 1e+07 replicates)
+
+data:  col 
+p-value = 1e-07
+alternative hypothesis: two.sided 
+
---
+++ subsetting bigdata.R
@@ -0,0 +1,159 @@
+###subsetting bigtable
+bigtable <- read.csv("ST2.csv")
+##get all KD estimates for each SH2 domain
+
+SHP2N <- subset(bigtable, SH2.domain=="SHP2N", select="Array.ln.KD")
+
+
+SHP2Nclean <- SHP2N[!is.na(SHP2N)]
+
+
+SHP2N <- subset(bigtable, SH2.domain=="SHP2N", select="Array.ln.KD")
+
+### don't want to spend the time figuring out how to average duplicates in datatable used excel
+SH2s <- read.csv("lnkds.csv")
+SHP2N <- SH2s$SHP2N[!is.na(SH2s$SHP2N)]
+NCK1 <- SH2s$NCK1[!is.na(SH2s$NCK1)]
+GRB2 <- SH2s$GRB2[!is.na(SH2s$GRB2)]
+ABL1 <- SH2s$ABL1[!is.na(SH2s$ABL1)]
+## can do many tests with this data type
+t.test(SHP2N,NCK1)
+t.test(SHP2N,ABL1)
+t.test(SHP2N,GRB2)
+wilcox.test(ABL1,GRB2)
+wilcox.test(ABL1,NCK1)
+wilcox.test(ABL1,SHP2N)
+t.test(ABL1,SHP2N)
+ks.test(ABL1,SHP2N)
+ks.test(ABL1,GRB2)
+ks.test(ABL1,NCK1)
+wilcox.test(ABL1,SHP2N)
+ks.test(ABL1,SHP2N, alternative="l")
+ks.test(ABL1,SHP2N, alternative="g")
+ks.test(ABL1,GRB2, alternative="g")
+ks.test(ABL1,NCK1, alternative="g")
+
+
+### but wait is my data normal??? apparently there is controversy over using this test. Ho: The data is drawn from a normal distribution
+## if p<.05 then reject this null
+shapiro.test(GRB2)##Reject p = .0376 NORMAL QQ plot looks pretty good though
+shapiro.test(NCK1)## DON'T REJECT
+shapiro.test(SHP2N)## DON'T REJECT
+shapiro.test(ABL1)## DON'T REJECT
+
+
+##Normal QQ plots for diagnostics, they all look pretty good to me....
+qqnorm(GRB2)
+qqnorm(NCK1)
+qqnorm(SHP2N)
+qqnorm(ABL1)
+## split plots
+
+
+
+### need to rearrange into two columns 1 is KDs and the other is SH2 domain (again in excel)
+KDANOVA <- read.csv("KDANOVA.csv")
+### create a boxplot for visualizing (note ABL1 is a tight distribution)
+boxplot(KDANOVA$KD ~ KDANOVA$SH2)
+
+### run anova MAY REQUIRE BALANCED DATA!
+SH2.aov <- aov(KDANOVA$KD ~ KDANOVA$SH2)
+summary(SH2.aov)
+### 'anova' function OK for unequal sample sizes! result is not significant (can't reject that the means are the same)but what is the power?
+anova(SH2.aov)
+
+## was not significant but if it was a multiple testing correction would be needed also see 'pairwise.t.test'
+
+TukeyHSD(SH2.aov)
+
+## Kruskal Wallis Test One Way Anova by Ranks nonparametric
+kruskal.test(KDANOVA$KD~KDANOVA$SH2) # where y1 is numeric and A is a factor 
+## result is NOT SIGNIFICANT therefore can't reject the null which is:such that the probability that a random observation from one group is greater than 
+#a random observation from another group is 0.5. 
+
+### Kolmogorov-smirnoff test on 4C2=6
+
+### anderson darling k-sample tests
+
+install.packages("kSamples")
+library(kSamples)
+### ad.test tests the null that This function uses the Anderson-Darling criterion to test the hypothesis that k independent samples
+## with sample sizes n1,n2,..nk arose from a common unspecified distribution function F (x)
+
+ad.out <- ad.test(NCK1,GRB2,ABL1,SHP2N,method="asymptotic")
+
+## RESULT IS SIGNIFICANT! So two-way tests? YES
+
+ad.outNG <- ad.test(NCK1,GRB2,method="asymptotic")
+ad.outNA <- ad.test(NCK1,ABL1,method="asymptotic")
+ad.outNS <- ad.test(NCK1,SHP2N,method="asymptotic")
+ad.outGA <- ad.test(GRB2,ABL1,method="asymptotic")
+ad.outGS <- ad.test(GRB2,SHP2N,method="asymptotic")
+ad.outAS <- ad.test(ABL1,SHP2N,method="asymptotic")
+
+## get p-values
+
+pNG <- ad.outNG$ad[1,3]
+pNA <- ad.outNA$ad[1,3]
+pNS <- ad.outNS$ad[1,3]
+pGA <- ad.outGA$ad[1,3]
+pGS <- ad.outGS$ad[1,3]
+pAS <- ad.outAS$ad[1,3]
+
+### insert into vector
+
+p <- c(pNG,pNA,pNS,pGA,pGS,pAS)
+
+### correct with multiple testing method
+
+p.adjust(p, method="bonferroni")
+
+### SIGNIFICANT IS ABL1-GRB2 AND ABL1-NCK1 with conservative bonferroni. ABL1-SHP2 IS VERY CLOSE! BH you can accept it! 
+
+###############overlaid qqplots with qlines###########
+
+##set the range for the qqplots
+
+ylim=range(ABL1,SHP2N,GRB2,NCK1)
+
+###plot each with lines intersecting the 1st and 3rd quantiles
+qqnorm(ABL1, ylim=ylim)
+qqline(ABL1)
+GRB2pts <- qqnorm(GRB2, plot.it=FALSE)
+points(GRB2pts, col=2)
+qqline(GRB2, col=2)
+SHP2Npts <- qqnorm(SHP2N, plot.it=FALSE)
+points(SHP2Npts, col=3)
+qqline(SHP2N, col=3)
+NCK1pts <- qqnorm(NCK1, plot.it=FALSE)
+points(NCK1pts, col=4)
+qqline(NCK1, col=4)
+
+##summary stats
+> summary(SHP2N)
+Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+0.3573  2.6440  3.6010  4.2610  6.5830  7.8780 
+> sd(SHP2N)
+[1] 2.338168
+> summary(ABL1)
+Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+1.612   3.111   3.963   4.045   4.715   6.892 
+> sd(ABL1)
+[1] 1.208477
+> summary(NCK1)
+Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+1.143   2.918   4.658   4.702   6.418   8.583 
+> sd(NCK1)
+[1] 2.079857
+> summary(GRB2)
+Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+-1.025   2.008   4.247   3.914   5.900   7.991 
+> sd(GRB2)
+[1] 2.430915
+
+
+
+
+
+
+
